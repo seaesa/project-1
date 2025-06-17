a3ab5446101b90927a120e4119b47d0f3a90301a
Ngoc Hai <hairipi100@gmail.com>
2025-06-17 21:49:24 +0700

    split to two case

---
+++ bai-1/src/app/app.routes.ts
@@ -0,0 +1,4 @@
+import { Routes } from '@angular/router';
+import { StudentComponent } from './student/student.component';
+
+export const routes: Routes = [{ path: '', component: StudentComponent }];
---
+++ bai-2/src/app/app.routes.ts
@@ -0,0 +1,4 @@
+import { Routes } from '@angular/router';
+import { BankComponent } from './bank/bank.component';
+
+export const routes: Routes = [{ path: '', component: BankComponent }];
